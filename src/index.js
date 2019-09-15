f0127e91d782349691ba855c254bb01659771b7a
Kim Konstantin <kimkonsts@gmail.com>
2019-09-15 13:27:23 +0300

    Create collisions and score count Add speedup after 30 seconds

--- src/index.js
+++ src/index.js
@@ -14,6 +14,40 @@ function queueUpdates(numTicks) {
     }
 }
 
+function checkBordersCollision(ball) {
+    if ((ball.x > canvas.width) || (ball.x < 0)) {
+        ball.vx = -ball.vx
+    }
+
+    if ((ball.y < 0)) {
+        ball.vy = -ball.vy
+    }
+    
+    if ((ball.y > canvas.height)) {
+        //stopGame(gameState.stopCycle)
+        ball.vy = -ball.vy
+    }
+
+}
+
+function checkPlatformCollision(ball) {
+    const player = gameState.player
+    if ((ball.x > player.x - gameState.player.width/2) && 
+        (ball.x < player.x + player.width/2) &&
+        (ball.y + ball.radius >= canvas.height - player.height)) {
+        ball.vy = -ball.vy
+        ball.vx += (ball.x - player.x) * 0.1
+    }
+}
+
+function checkCollisions(ball) {
+    checkPlatformCollision(ball)
+    checkBordersCollision(ball)
+
+    ball.y += ball.vy
+    ball.x += ball.vx
+}
+
 function draw(tFrame) {
     const context = canvas.getContext('2d');
 
@@ -22,6 +56,7 @@ function draw(tFrame) {
 
     drawPlatform(context)
     drawBall(context)
+    drawCount(context)
 }
 
 function update(tick) {
@@ -30,8 +65,27 @@ function update(tick) {
     gameState.player.x += vx
 
     const ball = gameState.ball
-    ball.y += ball.vy
-    ball.y += ball.vx
+    checkCollisions(ball)
+    
+    const count = gameState.count
+    addCount(count)
+    addSpeed(ball)
+
+}
+
+function addCount(count) {
+    if (gameState.lastTick - gameState.lastScoreTick >= 1000) {
+        count.score += 1
+        gameState.lastScoreTick = gameState.lastTick;
+    }
+}
+
+function addSpeed(ball) {
+    if (gameState.lastTick - gameState.lastSpeedupTick >= 30000) {
+        ball.vx *= 1.1
+        ball.vy *= 1.1
+        gameState.lastSpeedupTick = gameState.lastTick
+    }
 }
 
 function run(tFrame) {
@@ -71,6 +125,17 @@ function drawBall(context) {
     context.closePath();
 }
 
+function drawCount(context) {
+    const {x, y, width, height, score} = gameState.count;
+    context.beginPath()
+    context.rect(x+1, y, width , height)
+    context.strokeStyle = '#000000'
+    context.stroke()
+    context.closePath()
+    context.font = "14px serif";
+    context.fillText("Score: " + score, x + width/4, y + height/2);
+}
+
 function setup() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -78,6 +143,8 @@ function setup() {
     canvas.addEventListener('mousemove', onMouseMove, false);
 
     gameState.lastTick = performance.now();
+    gameState.lastScoreTick = gameState.lastTick;
+    gameState.lastSpeedupTick = gameState.lastTick;
     gameState.lastRender = gameState.lastTick;
     gameState.tickLength = 15; //ms
 
@@ -100,8 +167,15 @@ function setup() {
         x: canvas.width / 2,
         y: 0,
         radius: 25,
-        vx: 0,
+        vx: Math.random(1),
         vy: 5
+    }
+    gameState.count = {
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 50,
+        score: 0
     }
 }
 
